1ffd4591d24170264c7b148de9eace9ab0da079c
Rupinder Singh <inforupinder79@gmail.com>
2023-04-20 19:35:50 +0530

    not initial

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
-import Settings from './components/settings';
 
 function App() {
   return (
     <div className="App">
-        <h1>Quiz App</h1>
-      <Settings />
+        <h1>Quiz App</h1>      
     </div>
   );
 }
 
 export default App;
+
